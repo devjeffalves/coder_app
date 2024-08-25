4fa5c5172b4735b797efe3f2d0e3f917040fca6b
devjeffalves <jeff-alves@outlook.com>
2024-08-25 19:14:14 -0300

    app finalizado

---
+++ app-mobile/components/questionario/Opcao.tsx
@@ -0,0 +1,34 @@
+import { Pressable, StyleSheet } from "react-native"
+import { Text } from "react-native"
+
+
+
+export interface OpcaoProps {
+    indice: number
+    texto: string
+    onPress: () => void 
+}
+
+
+export default function Opcao(props: OpcaoProps) {
+    return (
+        <Pressable style={styles.container} onPress={props.onPress}>
+            <Text style={styles.texto}>{props.texto}</Text>
+        </Pressable>
+
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#2E9D48',
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        borderRadius: 50,
+    },
+    texto: {
+        color: 'white',
+        fontSize: 23,
+        fontWeight: 'bold',
+    },
+
+})
